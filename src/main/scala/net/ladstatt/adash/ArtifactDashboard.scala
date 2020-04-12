a12d6e34080b8d4ab9b6196043b7f6740b5f5cf0
Robert Ladstätter <rladstaetter@gmail.com>
2020-04-12 23:23:24 +0200

    adds basic rest interface calling via Ajax object
    shows how to call a rest service, along with mapping the result to case classes which can be used in the scala code.

--- src/main/scala/net/ladstatt/adash/ArtifactDashboard.scala
+++ src/main/scala/net/ladstatt/adash/ArtifactDashboard.scala
@@ -1,17 +1,71 @@
 package net.ladstatt.adash
 
-import org.scalajs.dom
 import org.scalajs.dom.document
+import org.scalajs.dom.ext.Ajax
+import org.scalajs.dom.raw.Element
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.scalajs.js
+import scala.scalajs.js.JSON
+import scala.util.{Failure, Success}
+
+@js.native
+trait CheckSum extends js.Object {
+  def md5: String
+
+  def sha1: String
+}
+
+@js.native
+trait Asset extends js.Object {
+  def id: String
+
+  def path: String
+
+  def downloadUrl: String
+
+  def repository: String
+
+  def format: String
+
+  def checksum: CheckSum
+}
+
+@js.native
+trait AssetsResult[T] extends js.Object {
+  def items: js.Array[T]
+
+  def continuationToken: String
+}
+
 
 object ArtifactDashboard {
 
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Paragraph filled via Scala.js")
+    queryNexus()
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    parNode.textContent = text
-    targetNode.appendChild(parNode)
+  def queryNexus(): Unit = {
+    // executioncontext needed for ajax call
+    implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
+
+    Ajax.get("http://127.0.0.1:8081/service/rest/v1/assets?repository=maven-public") onComplete {
+      case Success(v) =>
+        val assetsResult = JSON.parse(v.responseText).asInstanceOf[AssetsResult[Asset]]
+        for (a <- assetsResult.items) {
+          document.body.appendChild(p(a.id))
+        }
+        org.scalajs.dom.window.alert(assetsResult.continuationToken)
+      case Failure(e) =>
+        e.printStackTrace()
+        org.scalajs.dom.window.alert(e.getMessage)
+    }
   }
+
+  def p(text: String): Element = {
+    val p = document.createElement("p")
+    p.textContent = text
+    p
+  }
+
 }
