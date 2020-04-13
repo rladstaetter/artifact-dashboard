36086805a5edead83aac7bb6d7eb3ffe79de39fa
Robert Ladstätter <rladstaetter@gmail.com>
2020-04-13 19:10:39 +0200

    shows how to query Nexus rest api for a specific artifact by giving maven coordinates

--- src/main/scala/net/ladstatt/adash/ArtifactDashboard.scala
+++ src/main/scala/net/ladstatt/adash/ArtifactDashboard.scala
@@ -4,7 +4,7 @@ import org.scalajs.dom.document
 import org.scalajs.dom.ext.Ajax
 import org.scalajs.dom.raw.Element
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.scalajs.js
 import scala.scalajs.js.JSON
 import scala.util.{Failure, Success}
@@ -38,34 +38,66 @@ trait AssetsResult[T] extends js.Object {
   def continuationToken: String
 }
 
+case class MavenCoords(groupId: String
+                       , artifactId: String
+                       , baseVersion: String
+                       , extension: String
+                       , classifier: String)
 
 object ArtifactDashboard {
 
+  // executioncontext needed for ajax call
+  implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
+
+  // url of used nexus
+  val nexusBaseUrl: String = "http://127.0.0.1:8081/"
+
+  // rest url which we'll use to retrieve information about assets
+  val assetsSearchUri: String = "service/rest/v1/search/assets?"
+
+  private val artifact: MavenCoords = MavenCoords("net.ladstatt", "fx-animations", "1.0-SNAPSHOT", "jar", "jar-with-dependencies")
+
   def main(args: Array[String]): Unit = {
-    queryNexus()
-  }
-
-  def queryNexus(): Unit = {
-    // executioncontext needed for ajax call
-    implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
-
-    Ajax.get("http://127.0.0.1:8081/service/rest/v1/assets?repository=maven-public") onComplete {
-      case Success(v) =>
-        val assetsResult = JSON.parse(v.responseText).asInstanceOf[AssetsResult[Asset]]
-        for (a <- assetsResult.items) {
-          document.body.appendChild(p(a.id))
-        }
-        org.scalajs.dom.window.alert(assetsResult.continuationToken)
-      case Failure(e) =>
-        e.printStackTrace()
-        org.scalajs.dom.window.alert(e.getMessage)
+    queryNexus(artifact) onComplete {
+      case Failure(e) => org.scalajs.dom.window.alert(e.getMessage)
+      case Success(either) => either match {
+        case Left(e) => org.scalajs.dom.window.alert(e.getMessage)
+        case Right(downloadUrl) =>
+          document.body.appendChild(a(artifact.artifactId, downloadUrl))
+      }
     }
   }
 
-  def p(text: String): Element = {
-    val p = document.createElement("p")
-    p.textContent = text
-    p
+  def queryNexus(mvnCoords: MavenCoords): Future[Either[Throwable, String]] = {
+
+    val requestString = nexusBaseUrl + assetsSearchUri +
+      Map("repository" -> "testrepo-snapshot"
+        , "sort" -> "version"
+        , "direction" -> "desc"
+        , "maven.groupId" -> mvnCoords.groupId
+        , "maven.artifactId" -> mvnCoords.artifactId
+        , "maven.baseVersion" -> mvnCoords.baseVersion
+        , "maven.extension" -> mvnCoords.extension
+        , "maven.classifier" -> mvnCoords.classifier).map {
+        case (k, v) => s"$k=$v"
+      }.mkString("&")
+
+    Ajax.get(requestString).map {
+      xml =>
+        val assetsResult = JSON.parse(xml.responseText).asInstanceOf[AssetsResult[Asset]]
+        assetsResult.items.headOption match {
+          case None => Left(new RuntimeException("No artifact with given coordinates found: " + mvnCoords))
+          case Some(x) => Right(x.downloadUrl)
+        }
+    }
+
+  }
+
+  def a(text: String, href: String): Element = {
+    val a = document.createElement("a")
+    a.textContent = text
+    a.setAttribute("href", href)
+    a
   }
 
 }
